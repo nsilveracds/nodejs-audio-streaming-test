942ad2d00ee2763c0b3c51feb31a584bb2f08af5
Nahuel silvera <nsilvera@codigodelsur.com>
2024-09-18 14:47:03 -0300

    stream audio via hls

--- index.js
+++ index.js
@@ -1,55 +1,25 @@
-const {parseFile} = require('music-metadata');
-const express = require("express")
-const Throttle = require('throttle')
-const Fs = require('fs')
-const { PassThrough } = require('stream')
-const app = express();
+const http = require('http');
+const fs = require('fs');
+const port = 8000;
 
-let filePath = "./public/portwave - shadow lady (slowed).mp3"; // put your favorite audio file
-let bitRate = 0;
-const streams = new Map()
-app.use(express.static('public'));
-
-
-app.get("/stream", (req, res) => {
-    const { id, stream } = generateStream() // We create a new stream for each new client
-    res.setHeader("Content-Type", "audio/mpeg")
-    stream.pipe(res) // the client stream is pipe to the response
-    res.on('close', () => { streams.delete(id) })
-})
-
-
-const init = async () => {
-    const fileInfo = await parseFile(filePath)
-    bitRate = fileInfo.format.bitrate / 8
-
-}
-
-const playFile = (filePath) => {
-    const songReadable = Fs.createReadStream(filePath);
-    const throttleTransformable = new Throttle(bitRate);
-    songReadable.pipe(throttleTransformable);
-    throttleTransformable.on('data', (chunk) => { broadcastToEveryStreams(chunk) });
-    throttleTransformable.on('error', (e) => console.log(e))
-
-}
-const broadcastToEveryStreams = (chunk) => {
-    for (let [id, stream] of streams) {
-        stream.write(chunk) // We write to the client stream the new chunck of data
-    }
-}
-
-const generateStream = () => {
-    const id = Math.random().toString(36).slice(2);
-    const stream = new PassThrough()
-    streams.set(id, stream)
-    return { id, stream }
-}
-
-init()
-    .then(() => {
-        app.listen(8080)
-        console.log("listening at http://localhost:8080")
-        console.log('streaming at http://localhost:8080/stream')
-    })
-    .then(() => playFile(filePath))
+http.createServer(function (request, response) {
+    console.log('request starting...');
+    const filePath = './temp/chunks' + request.url;
+    fs.readFile(filePath, function (error, content) {
+        response.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
+        if (error) {
+            if (error.code == 'ENOENT') {
+                fs.readFile('./404.html', function (error, content) {
+                    response.end(content, 'utf-8');
+                });
+            } else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+'..\n');
+                response.end();
+            }
+        } else {
+            response.end(content, 'utf-8');
+        }
+    });
+}).listen(port);
+console.log(`Server running at http://127.0.0.1:${port}/`);
